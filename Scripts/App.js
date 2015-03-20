73f23e580f2ffe4de665d256b770ef8ccfd3fef5
Frederick-S <mao_xiaodan@hotmail.com>
2015-03-20 11:16:53 +0800

    Update App.js

--- Scripts/App.js
+++ Scripts/App.js
@@ -92,13 +92,13 @@
         var renderer = new marked.Renderer();
         renderer.table = function (header, body) {
             return '<table class=\'table\'>\n'
-              + '<thead>\n'
-              + header
-              + '</thead>\n'
-              + '<tbody>\n'
-              + body
-              + '</tbody>\n'
-              + '</table>\n';
+                + '<thead>\n'
+                + header
+                + '</thead>\n'
+                + '<tbody>\n'
+                + body
+                + '</tbody>\n'
+                + '</table>\n';
         };
 
         var html = marked(markdown, { renderer: renderer });
